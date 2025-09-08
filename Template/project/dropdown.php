846709d9399b118ce3efd4e53952325d8fb800b3
Fredrik M. Johansen <143546239+FredrikJohansen@users.noreply.github.com>
2025-09-08 18:09:31 +0200

    Updated to 1.2.0

--- Template/project/dropdown.php
+++ Template/project/dropdown.php
@@ -4,7 +4,7 @@
     </a>
 </li>
 <li>
-    <a href="/?controller=GanttExportController&action=export&project_id=<?= $project['id'] ?>&plugin=GanttExport&hide_today=1" title="<?= t('Export Gantt chart without today indicator (Print to PDF)') ?>">
-        <i class="fa fa-file-o"></i> <?= t('Export Gantt Chart (No Today Line)') ?>
+    <a href="/?controller=GanttExportController&action=export&project_id=<?= $project['id'] ?>&plugin=GanttExport&hide_today=1" title="<?= t('Export Gantt chart without NOW indicator (Print to PDF)') ?>">
+        <i class="fa fa-file-o"></i> <?= t('Export Gantt Chart (No NOW indicator)') ?>
     </a>
 </li>
